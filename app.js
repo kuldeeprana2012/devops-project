450bb830c802d9809082096aa55855a2ada83706
Kuldeep_Rana_IT <kuldeepranait@gmail.com>
2025-09-21 16:49:43 +0530

    new text added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello DevOps from Jenkins + Docker + Git + RHEL 8!'));
+app.get('/', (req, res) => res.send('helo this is kuldeep rna and resurmt he onitering for this '));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
 
